00ec5739880107af91dad64e4ee37b93a7611c8d
Nikolas Newman <nik@newmancj.com>
2013-04-19 19:22:44 +0100

    Coded

---
+++ common/McAddonOverhal/lib/Reference.java
@@ -0,0 +1,8 @@
+package McAddonOverhal.lib;
+
+public class Reference {
+    
+    public static final String MOD_ID = "MAOver";
+    public static final String MOD_NAME = "McAddon";
+    public static final String MOD_VERSION = "0.0.1";
+}
